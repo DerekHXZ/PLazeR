6382525d938bf5d1d1d2b4a7c15d50d66d8b864f
Peiqian Li <pl2521@columbia.edu>
2015-05-09 20:12:10 -0400

    Gaussian now compiles

--- lab3-qsys/arit.sv
+++ lab3-qsys/arit.sv
@@ -13,6 +13,8 @@ module arit
   output [7:0]out_pixel   //output to output logic block
 );
 
+logic[15:0] coefficients[0:288] = '{16'd0, 16'd0, 16'd1, 16'd1, 16'd2, 16'd3, 16'd3, 16'd4, 16'd4, 16'd4, 16'd3, 16'd3, 16'd2, 16'd1, 16'd1, 16'd0, 16'd0, 16'd0, 16'd1, 16'd1, 16'd2, 16'd4, 16'd6, 16'd8, 16'd9, 16'd10, 16'd9, 16'd8, 16'd6, 16'd4, 16'd2, 16'd1, 16'd1, 16'd0, 16'd1, 16'd1, 16'd3, 16'd5, 16'd8, 16'd12, 16'd16, 16'd19, 16'd20, 16'd19, 16'd16, 16'd12, 16'd8, 16'd5, 16'd3, 16'd1, 16'd1, 16'd1, 16'd2, 16'd5, 16'd9, 16'd15, 16'd22, 16'd29, 16'd34, 16'd36, 16'd34, 16'd29, 16'd22, 16'd15, 16'd9, 16'd5, 16'd2, 16'd1, 16'd2, 16'd4, 16'd8, 16'd15, 16'd25, 16'd36, 16'd48, 16'd57, 16'd60, 16'd57, 16'd48, 16'd36, 16'd25, 16'd15, 16'd8, 16'd4, 16'd2, 16'd3, 16'd6, 16'd12, 16'd22, 16'd36, 16'd54, 16'd71, 16'd84, 16'd89, 16'd84, 16'd71, 16'd54, 16'd36, 16'd22, 16'd12, 16'd6, 16'd3, 16'd3, 16'd8, 16'd16, 16'd29, 16'd48, 16'd71, 16'd94, 16'd111, 16'd117, 16'd111, 16'd94, 16'd71, 16'd48, 16'd29, 16'd16, 16'd8, 16'd3, 16'd4, 16'd9, 16'd19, 16'd34, 16'd57, 16'd84, 16'd111, 16'd131, 16'd138, 16'd131, 16'd111, 16'd84, 16'd57, 16'd34, 16'd19, 16'd9, 16'd4, 16'd4, 16'd10, 16'd20, 16'd36, 16'd60, 16'd89, 16'd117, 16'd138, 16'd146, 16'd138, 16'd117, 16'd89, 16'd60, 16'd36, 16'd20, 16'd10, 16'd4, 16'd4, 16'd9, 16'd19, 16'd34, 16'd57, 16'd84, 16'd111, 16'd131, 16'd138, 16'd131, 16'd111, 16'd84, 16'd57, 16'd34, 16'd19, 16'd9, 16'd4, 16'd3, 16'd8, 16'd16, 16'd29, 16'd48, 16'd71, 16'd94, 16'd111, 16'd117, 16'd111, 16'd94, 16'd71, 16'd48, 16'd29, 16'd16, 16'd8, 16'd3, 16'd3, 16'd6, 16'd12, 16'd22, 16'd36, 16'd54, 16'd71, 16'd84, 16'd89, 16'd84, 16'd71, 16'd54, 16'd36, 16'd22, 16'd12, 16'd6, 16'd3, 16'd2, 16'd4, 16'd8, 16'd15, 16'd25, 16'd36, 16'd48, 16'd57, 16'd60, 16'd57, 16'd48, 16'd36, 16'd25, 16'd15, 16'd8, 16'd4, 16'd2, 16'd1, 16'd2, 16'd5, 16'd9, 16'd15, 16'd22, 16'd29, 16'd34, 16'd36, 16'd34, 16'd29, 16'd22, 16'd15, 16'd9, 16'd5, 16'd2, 16'd1, 16'd1, 16'd1, 16'd3, 16'd5, 16'd8, 16'd12, 16'd16, 16'd19, 16'd20, 16'd19, 16'd16, 16'd12, 16'd8, 16'd5, 16'd3, 16'd1, 16'd1, 16'd0, 16'd1, 16'd1, 16'd2, 16'd4, 16'd6, 16'd8, 16'd9, 16'd10, 16'd9, 16'd8, 16'd6, 16'd4, 16'd2, 16'd1, 16'd1, 16'd0, 16'd0, 16'd0, 16'd1, 16'd1, 16'd2, 16'd3, 16'd3, 16'd4, 16'd4, 16'd4, 16'd3, 16'd3, 16'd2, 16'd1, 16'd1, 16'd0, 16'd0};
+
 byte pixels[289];
 
 genvar i, j;
@@ -21,32 +23,30 @@ begin
 	for(i = 0; i < 17; i+=1) begin: init_i
 		for(j = 0; j < 17; j+=1) begin: init_j
 		always_comb begin
-			pixels[i*j] <= in_pixels[(i*17+j)*8+7 : (i*17+j)*8];
+			pixels[i*17+j] <= in_pixels[(i*17+j)*8+7 : (i*17+j)*8];
 		end
 	end
 	end
 	end
 endgenerate
 
+logic[15:0] val[0:289], coesum[0:289];
+assign val[0] = 16'b0;
+assign coesum[0] = 16'b0;
+
 genvar di, dj;
 generate
 begin
-	real val = 0, wsum = 0;
 	for(di = -8; di <= 8; di+=1) begin: init_di
-		for(dj = -8; dj <= 8; dj+=1) begin: init_dj			
-			int dsq = di * di + dj * dj;
-			real w = exp(-dsq / 2.0) / 6.28;
-			
-			always_comb begin
-				int index = (di+8)*17 + dj+8;
-				val += pixels[index] * w;
-				wsum += w;
-			end
+		for(dj = -8; dj <= 8; dj+=1) begin: init_dj
+			localparam integer index = (di+8)*17 + dj+8;
+			assign val[index+1] = val[index] + pixels[index] * coefficients[index];
+			assign coesum[index+1] = coesum[index] + coefficients[index];
 		end
 	end
 end
 endgenerate
 
-assign out_pixel = round(val/wsum);
+assign out_pixel = val[289]/coesum[289];
 
 endmodule
