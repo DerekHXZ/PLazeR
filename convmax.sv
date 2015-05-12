55072115c56b012171c012f596196c0d91cee052
Robert Ying <rbtying@aeturnalus.com>
2015-05-12 08:05:03 -0400

    process_device as 32-bit bus

--- convmax.sv
+++ convmax.sv
@@ -1,6 +1,6 @@
 module convmax(
     input logic                 clk,
-    input logic [119:0][7:0]    indata,
+    input logic [143:0][7:0]    indata,
     input logic [7:0][7:0]      gauss,
 
     output logic [15:0]         maxval,
@@ -8,38 +8,32 @@ module convmax(
     output logic                ready
 );
 
-//conv c1(.clk        (clk),
-  //      .data       (indata[15:0]),
-    //    .gauss      (gauss),
-      //  .convvalue  (maxval)
-        //);
-logic[15:0] val[0:103];
-logic[15:0] maxval_so_far[0:103];
-logic[7:0] maxpos_so_far[0:103];
+logic[15:0] val[0:127];
+logic[15:0] maxval_so_far[0:127];
+logic[7:0] maxpos_so_far[0:127];
 
-assign maxval_so_far[0] = val[0];
+assign maxval_so_far[0] = 16'b0;
 assign maxpos_so_far[0] = 8'b0;
 
 genvar i;
 generate
 begin
-	for( i = 0; i < 104; i++ ) begin: for_i
-		conv (.clk        (clk),       //input
-		.data       (indata[i+15:i]),     // input
-		.gauss      (gauss),              //input
-		.convvalue  (val[i])     // output [16:0]  convvalue
-		);
-		
-		if (i>0) begin
-			assign maxval_so_far[i] = (maxval_so_far[i-1] > val[i]) ? maxval_so_far[i-1] : val[i];
-			assign maxpos_so_far[i] = (maxval_so_far[i-1] > val[i]) ? maxpos_so_far[i-1] : i;
-		end
-	end
+    for( i = 0; i < 128; i++ ) begin: for_i
+        conv(.clk        (clk),       //input
+             .data       (indata[i+15:i]),     // input
+             .gauss      (gauss),              //input
+             .convvalue  (val[i])     // output [16:0]  convvalue
+        );
+        if (i>0) begin
+            assign maxval_so_far[i] = (maxval_so_far[i-1] > val[i]) ? maxval_so_far[i-1] : val[i];
+            assign maxpos_so_far[i] = (maxval_so_far[i-1] > val[i]) ? maxpos_so_far[i-1] : i;
+        end
+    end
 end
 endgenerate
 
-assign max_val = maxval_so_far[103];
-assign maxpos = maxpos_so_far[103];
+assign max_val = maxval_so_far[127];
+assign maxpos = maxpos_so_far[127];
 assign ready = 1;
 
 endmodule
--- process_device.sv
+++ process_device.sv
@@ -3,40 +3,54 @@ module process_device(
     input logic                 clk,
     input logic                 write,
     input logic                 read,
-    input logic [7:0]           address,
-    input logic [127:0]         byteenable,
-    input logic [1023:0]        writedata,
-    output logic [1023:0]       readdata,
+    input logic [10:0]          address,
+    input logic [3:0]           byteenable,
+    input logic [31:0]          writedata,
+    output logic [31:0]         readdata,
     output logic                waitrequest
 );
 
-// writedata layout
+// memory layout
 // address = 0x00
-// byteenable = 111111111...
+// byteenable = 1111
 // [image data ][gaussian ]
-// [120 x 8 bit][8 x 8 bit]
-// [0.......959][960..1023]
+// [144 x 8 bit][8 x 8 bit  ]
+// [0......1151][1152...1216]
 // note that this is only the left half of the gaussian, since
 // the gaussian kernel is symmetric about 0
 
 // readdata layout
-// address = 0x01
-// byteenable = 111000000...
+// address = 0x00
+// byteenable = 1110
 // [1 x 16 bit][1 x 8 bit]
 // [0.......15][16.....23]
 
-logic [15:0]    maxval;
-logic [7:0]     maxpos;
-logic           ready;
+logic [15:0]        maxval;
+logic [7:0]         maxpos;
+logic               ready;
+logic [151:0][7:0] memory;
 
 convmax cm1(.clk        (clk),
-            .indata     (writedata[959:0]),
-            .gauss      (writedata[1023:960]),
+            .indata     (memory[143:0]),
+            .gauss      (memory[151:144]),
             .maxval     (maxval),
             .maxpos     (maxpos),
             .ready      (ready)
             );
 
+
+always_ff @(posedge clk)
+begin
+	if (reset) memory <= 0;
+	if (write) begin
+		if (byteenable[0]) memory[address] <= writedata[7:0];
+		if (byteenable[1]) memory[address+1] <= writedata[15:8];
+		if (byteenable[2]) memory[address+2] <= writedata[23:16];
+		if (byteenable[3]) memory[address+3] <= writedata[31:24];
+	end
+end
+
+assign waitrequest = ~ready;
 assign readdata[15:0] = maxval;
 assign readdata[23:16] = maxpos;
 
